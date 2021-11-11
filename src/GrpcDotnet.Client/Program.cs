5004e65760b8722e322cf054c569720c8bf07019
Mikael Berrin <mikael.berrin@iver.se>
2021-11-11 19:24:01 +0100

    Add project files.

---
+++ src/GrpcDotnet.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcDotnet.Proto;
+
+namespace GrpcDotnet.Client;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://grpcdotnet.service");
+        var client = new Greeter.GreeterClient(channel);
+
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+        Console.WriteLine("Greeting: " + reply.Message);        
+    }
+}
